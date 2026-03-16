a2f8d28cd5ec1290b4a5722d95a60de003ca0b0f
Andrei <banakh@Andreis-MacBook-Air.local>
2026-03-16 20:07:57 +0300

    fix api callback

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://web-backend-lab-1drlinggg.onrender.com',
         changeOrigin: true,
       },
     },
